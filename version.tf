ea4a4e1c60d16e023ae7ed5c5b4e2fa0499c9245
chrism1 <chrism1@us.ibm.com>
2024-04-17 15:40:07 -0400

    updated tf providers to include ibm and hashicorp

--- version.tf
+++ version.tf
@@ -1,12 +1,15 @@
 terraform {
   required_version = ">= 1.3.0, <1.7.0"
-  # If your module requires any terraform providers, uncomment the "required_providers" section below and add all required providers.
-  # Each required provider's version should be a flexible range to future proof the module's usage with upcoming minor and patch versions.
 
-  #  required_providers {
-  #    ibm = {
-  #      source  = "IBM-Cloud/ibm"
-  #      version = ">= 1.64.0, < 2.0.0"
-  #    }
-  #  }
+  # Use a flexible range in modules that future proofs the module's usage with upcoming minor and patch versions
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.61.0, < 2.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1, < 1.0.0"
+    }
+  }
 }
